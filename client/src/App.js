86226a6bac6f8e560cb20a8b6a36a0c3d56e2396
shivambirla139 <shivambirla139@gmail.com>
2023-09-29 13:31:59 +0530

    a

--- client/src/App.js
+++ client/src/App.js
@@ -8,34 +8,12 @@ import SignUpForm from './Components/login/SignupForm';
 import { getUserData } from './services/api';
 function App() {
 
-    const [accessToken,setAccessToken] = useState(null);
-    const [user,setUser] = useState(null);
-    useEffect(()=>{
-        var useraccesstoken = sessionStorage.getItem('useraccesstoken');
-        async function getUser(){
-            if(useraccesstoken!=null){
-                try{
-                    const response = await getUserData();
-                    console.log(response);
-                }catch (e){
-                    console.log(e.message);
-                }
-            }
-        }
-        getUser();
-        setAccessToken(useraccesstoken);
-    },[]);
-
-    function changeStates(obj){
-      setAccessToken(obj.accessToken);
-    }
-
   return (
     <div className='App'>
       <BrowserRouter>
           <NavBar user={user}/>
           <Routes>
-            <Route path ="/login" element = {<LoginForm changeStates={changeStates} /> } />
+            <Route path ="/login" element = {<LoginForm  /> } />
             <Route path ="/signup" element = {<SignUpForm/>}/>
 
           </Routes>
