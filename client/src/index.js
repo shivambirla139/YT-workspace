25224e9dec06c28cb43398ccdf2c2c2e5c3731d0
shivambirla139 <shivambirla139@gmail.com>
2023-09-30 09:49:30 +0530

    juh

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,32 @@ import './index.css';
 import App from './App';
 import {Provider} from 'react-redux';
 import rootReducer from './state/rootReducer';
-import { createStore } from 'redux'
+import { configureStore } from "@reduxjs/toolkit";
 
-const store = createStore(rootReducer)
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
